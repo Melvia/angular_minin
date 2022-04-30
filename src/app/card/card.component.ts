acf6c89b6f9d17890774836d964f4e7ea848d63d
Nata <melvia.nr@gmail.com>
2022-04-30 18:31:10 +0500

    two_way_binding

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,27 @@
+import { style } from "@angular/animations";
+import { Component, OnInit } from "@angular/core"
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+   styleUrls:['./card.component.scss'],
+   interpolation: ['{{', '}}']
+
+})
+
+export class CardComponent implements OnInit {
+    title: string = 'My Card title';
+    text: string = 'My sample text';
+    imgUrl: string = 'https://angular.io/assets/images/logos/angular/angular.svg';
+    disabled = false;
+
+    ngOnInit() {}
+    changeTitle() {
+        this.title = "Title has been changed"
+    }
+    inputHandler(event: any) {
+        const value = event.target.value;
+        this.title = value;
+    }
+
+
+}
